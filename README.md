05de22543d168077f3b127c29f23d9107b3b1e30
Sumit <sumitbhagat498@gmail.com>
2022-06-20 16:42:28 +0530

    save all changes made for setup.py file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project_housing
 Machine Learning Project For House Price Prediction
+
+### after creating setup.py file write a command
+""" python setup.py install """
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from typing import List
+
+## declearing variables for the setup function
+
+PROJECT_NAME = "housing_predictor"
+VERSION = "0.0.1"
+AUTHOR = "Sumit Bhagat"
+DESCRIPTION = "This is the project to predict house price in bangalore"
+PACKAGES = ['housing']
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+"""
+Description - This function is going to return list of requirements mention in requirements.txt file.
+
+return: this function is going to return a list which contains the list of libraries
+mentioned in requiremnts.txt file.
+"""
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+setup(
+
+name = PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+install_requires=get_requirements_list()
+
+)
