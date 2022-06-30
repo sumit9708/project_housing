b566aabe1c3e105129ea14300765fa009bcf9302
Sumit <sumitbhagat498@gmail.com>
2022-06-30 12:09:27 +0530

    Updates on configuraton file

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -27,7 +27,7 @@ class Configuration:
 
     def get_data_ingestion_config(self)->DataIngestionConfig:
         try:
-            artifact_dir = TrainingPipelineConfig.artifact_dir
+            artifact_dir = self.training_pipeline_config.artifact_dir
             data_ingestion_artifact_dir = os.path.join(
                 artifact_dir,
                 DATA_INGESTION_ARTIFACT_DIR,
@@ -35,7 +35,7 @@ class Configuration:
             )
             data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
 
-            dataset_download_url = data_ingestion_config[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
             tgz_download_dir = os.path.join(data_ingestion_artifact_dir,
             data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
             )
