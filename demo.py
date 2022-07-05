4d9120273e7d4b4e4ef8c37c7e706305a9b556f1
Sumit <sumitbhagat498@gmail.com>
2022-07-05 12:33:11 +0530

    Data validation Configuration completed successfuly

--- demo.py
+++ demo.py
@@ -2,12 +2,14 @@ from housing.exception import HousingException
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.config.configuration import Configuration
 
 def main():
     try:
-
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)
+        #pipeline = Pipeline()
+        #pipeline.run_pipeline()
     except Exception as e:
         logging.error(f"{e}")
         print(e)
