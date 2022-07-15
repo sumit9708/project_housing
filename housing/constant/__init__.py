92c2444bf138679f0420b1d1561cb9b4f582b3f6
Sumit <sumitbhagat498@gmail.com>
2022-07-15 11:55:20 +0530

    Model trained and tested successfuly

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -3,13 +3,17 @@
 from datetime import datetime
 import os
 
-from housing.logger import CURRENT_TIME_STAMP
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
 
 ROOT_DIR = os.getcwd()  ## To get current working Directory
 CONFIG_DIR = "config"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+## CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
 
 ## Training pipeline related variable
 
